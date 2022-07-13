ee2729e70a74d6a932172244a047bee109e2a53a
kristjanthor <kristjanthor@dohop.com>
2022-07-13 17:54:50 +0000

    cleanup after layout changes

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Sidebar from "../components/Sidebar";
-import Player from "../components/Player";
 
 const Home: NextPage = () => {
   return <h1 className="text-white">Your Library</h1>;
--- pages/playlists/[...playlistId].tsx
+++ pages/playlists/[...playlistId].tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-import Sidebar from "../../components/Sidebar";
 import Center from "../../components/Center";
-import Player from "../../components/Player";
 
 const PlaylistPage: NextPage = () => {
   return <Center />;
