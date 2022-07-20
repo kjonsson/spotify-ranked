f7b0d56af9b4e0db6f6fc097102a09bf6865d84c
kristjanthor <kristjanthor@dohop.com>
2022-07-20 23:00:57 +0000

    remove home and library

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,22 +8,10 @@ const Sidebar = () => {
   return (
     <div className="pb-36 h-screen p-5 overflow-y-scroll text-xs text-gray-500 lg:text-sm sm:max-w-[12rem lg:max-w-[15rem]">
       <div className="space-y-4">
-        <Link href="/">
-          <a className="flex items-center space-x-2 hover:text-white">
-            <HomeIcon className="w-5 h-5" />
-            <p>Home</p>
-          </a>
-        </Link>
         <Link href="/search">
           <a className="flex items-center space-x-2 hover:text-white">
             <SearchIcon className="w-5 h-5" />
             <p>Search</p>
-          </a>
-        </Link>
-        <Link href="/library">
-          <a className="flex items-center space-x-2 hover:text-white">
-            <LibraryIcon className="w-5 h-5" />
-            <p>Your Library</p>
           </a>
         </Link>
         <hr className="border-t-[0.1px] border-gray-900" />
