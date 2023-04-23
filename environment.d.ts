4d030baffbd7d6f21d03cee99569fa4045029490
kjonsson <kristjanj11@gmail.com>
2023-04-23 08:52:28 +0000

    env

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const environmentSchema = z.object({
     NEXT_CLIENT_ID: z.string(),
     NEXT_CLIENT_SECRET: z.string(),
+    NEXTAUTH_SECRET: z.string(),
 });
 
 environmentSchema.parse(process.env);
